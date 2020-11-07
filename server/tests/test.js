53caf4d0079563eb4f6d9c65455dda3db3c0d2c1
ShuTingY <shuting_y@u.nus.edu>
2020-11-07 17:44:36 +0800

    update test

--- server/tests/test.js
+++ server/tests/test.js
@@ -1,7 +1,7 @@
 import chai from 'chai';
 import chatHttp from 'chai-http';
 import 'chai/register-should';
-import {app} from '../app';
+import {app} from '../index';
 
 chai.use(chatHttp);
 const { expect } = chai;
