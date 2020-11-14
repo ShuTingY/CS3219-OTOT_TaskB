0189f60780090ba26ef0675a5067d4048fd762ae
ShuTingY <shuting_y@u.nus.edu>
2020-11-14 17:36:56 +0800

    fix CD

--- server/config/config.js
+++ server/config/config.js
@@ -36,7 +36,7 @@ module.exports = {
     dialect: 'postgres',
     dialectOptions: {
       ssl: {
-        require: false,
+        require: true,
         rejectUnauthorized: false,
       },
     },
