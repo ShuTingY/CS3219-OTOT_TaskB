79a50a821636874d4ed7c07dd1352b604e8f7101
ShuTingY <shuting_y@u.nus.edu>
2020-11-14 20:46:59 +0800

    remove unused component

--- web/src/components/Todo.js
+++ web/src/components/Todo.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
-import { Segment, Card } from 'semantic-ui-react'
+import { Segment} from 'semantic-ui-react'
 import TodoItem from './TodoItem';
 import PropTypes from 'prop-types';
 
 class Todos extends Component {
     render() {
         return (
-        <Segment.Group centered textAlign='center' style={{marginTop:"1em"}} >
+        <Segment.Group style={{marginTop:"1em"}} >
           {this.props.todos.map((todo) => (
               <TodoItem key={todo.id} todo={todo} 
               markComplete={this.props.markComplete} 
