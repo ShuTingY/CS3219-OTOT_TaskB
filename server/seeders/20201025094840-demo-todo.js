cc799f6666440639aeceb428e3240f072498de09
ShuTingY <shuting_y@u.nus.edu>
2020-11-14 19:35:09 +0800

    update npm start CLi

--- server/seeders/20201025094840-demo-todo.js
+++ server/seeders/20201025094840-demo-todo.js
@@ -2,23 +2,47 @@
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    /**
-     * Add seed commands here.
-     *
-     * Example:
-     * await queryInterface.bulkInsert('People', [{
-     *   name: 'John Doe',
-     *   isBetaMember: false
-     * }], {});
-    */
+    await queryInterface.bulkInsert('Todos', [
+      {
+        "title": "Project team",
+        "description": "CS2020 due on 4th Oct",
+        "isCompleted": false,
+        "createdAt": new Date(),
+        "updatedAt": new Date()
+      },
+      {
+        "title": "Laundry",
+        "isCompleted": false,
+        "createdAt": new Date(),
+        "updatedAt": new Date()
+      },
+      {
+        "title": "Buy essentials",
+        "description":"Orange, Cereal, Mike, Shampoos",
+        "isCompleted": true,
+        "createdAt": new Date(),
+        "updatedAt": new Date()
+      },
+      {
+        "title": "Watch movie",
+        "description":"30th Oct, 9pm",
+        "isCompleted": false,
+        "createdAt": new Date(),
+        "updatedAt": new Date()
+      },
+      {
+        "title": "Interview",
+        "description":"14th Nov, 8am-9am",
+        "isCompleted": false,
+        "createdAt": new Date(),
+        "updatedAt": new Date()
+      },
+   
+    ], {});
+    
   },
 
   down: async (queryInterface, Sequelize) => {
-    /**
-     * Add commands to revert seed here.
-     *
-     * Example:
-     * await queryInterface.bulkDelete('People', null, {});
-     */
+    await queryInterface.bulkDelete('Todos', null, {});
   }
 };
