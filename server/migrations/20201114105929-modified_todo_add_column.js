54cdd4ccb2f3fd23bf5f7b7dfdef69dd84821681
ShuTingY <shuting_y@u.nus.edu>
2020-11-14 19:01:32 +0800

    update migrations

---
+++ server/migrations/20201114105929-modified_todo_add_column.js
@@ -0,0 +1,38 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Todos', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      title: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      description: {
+        type: Sequelize.STRING,
+        allowNull: true
+      },
+      isCompleted: {
+        type: Sequelize.BOOLEAN,
+        defaultValue:false
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Todos');
+  }
+};
